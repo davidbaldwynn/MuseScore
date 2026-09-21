c4c4604900b4b4255ceb095a35f4da425b0b06aa
davidbaldwynn <polydaic@gmail.com>
2026-09-22 00:26:28 +0300

    build: add Android core file sharing support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(composeBom)
     androidTestImplementation(composeBom)
     implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
